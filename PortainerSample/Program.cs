94c368d56eea75e1287e6918d3eefe19f889f3af
slothman5566 <ccps960316@gmail.com>
2024-04-04 20:30:03 +0800

    ✨update swagger route

--- PortainerSample/Program.cs
+++ PortainerSample/Program.cs
@@ -11,12 +11,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 
-app.MapGet("", () =>
+app.MapGet("api/v1/test", () =>
 {
     return "Hello World!!";
 })
